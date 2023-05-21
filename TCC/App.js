c8bd4228c5dfe3fc9c71d23de82875fa5aeca33f
Luiz Palhano <luiz.palhano@neopath.com.br>
2023-05-21 11:50:04 -0300

    Prática com elementos de React Native

--- TCC/App.js
+++ TCC/App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
+//Função root; rodada sempre e sempre primeiro
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {styles.backgroundStyle}>
+      <View style = {{padding: '8%'}}></View>
+      <View style = {{alignItems: 'center'}}>
+        <Text style = {styles.textStyle}>Hello, world!</Text>
+      </View>
     </View>
   );
 }
 
+//Stylesheet guarda os estilos do app
 const styles = StyleSheet.create({
-  container: {
+  //Estilo geral de fundo de tela
+  backgroundStyle: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'cyan'
   },
+
+  //Estilo geral de botão
+  buttonStyle: {
+    
+  },
+
+  //Estilo geral de caixa de input
+  inputStyle: {
+    flexDirection: 'column',
+    borderWidth: 2,
+    borderColor: 'gray',
+  },
+
+  //Estilo geral de bloco de texto
+  textStyle: {
+    padding: 15,
+    backgroundColor: '#0062ff',
+    textAlign: 'center',
+    width: '50%'
+  }
 });
