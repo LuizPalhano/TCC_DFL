b9b3fd5326f3aa407c5b202edbe776cf1f319202
Luiz Palhano <luiz.palhano@neopath.com.br>
2024-02-09 13:34:14 -0300

    Implementação inicial da execução repetida dos algoritmos

--- Code scraps/picker_algorithms_DFL.py
+++ Code scraps/picker_algorithms_DFL.py
@@ -1,6 +1,7 @@
 import graph_DFL as grafo
 import random
 import copy
+import math
 
 '''
 Estabelece os objetos a serem incluídos no grafo
@@ -19,6 +20,10 @@ node_C_inc = set()
 node_C_out = set()
 node_C = grafo.Node("C", node_C_inc, node_C_out)
 
+####
+#Breakpoint de três nós
+####
+#'''
 node_D_inc = set()
 node_D_out = set()
 node_D = grafo.Node("D", node_D_inc, node_D_out)
@@ -35,6 +40,10 @@ node_G_inc = set()
 node_G_out = set()
 node_G = grafo.Node("G", node_G_inc, node_G_out)
 
+####
+#Breakpoint de sete nós
+####
+#'''
 node_H_inc = set()
 node_H_out = set()
 node_H = grafo.Node("H", node_H_inc, node_H_out)
@@ -46,6 +55,10 @@ node_I = grafo.Node("I", node_I_inc, node_I_out)
 node_J_inc = set()
 node_J_out = set()
 node_J = grafo.Node("J", node_J_inc, node_J_out)
+####
+#Breakpoint de dez nós
+####
+#'''
 
 newGraph = grafo.Graph()
 
@@ -62,6 +75,7 @@ newGraph.addNode(node_J.name, node_J.incomingNeighbors, node_J.outgoingNeighbors
 
 #Função auxiliar de checagem de completude do grafo
 #Quando o grafo é completo, todas as cardinalidades são iguais a N - 1; se uma for diferente, não está completo
+#Complexidade: O(n) (itera uma vez por cada nó do grafo)
 def checkCompleteness(candidateGraph):
     cardinalities = candidateGraph.getCardinalityList()
     highestCardinality = candidateGraph.graphSize - 1
@@ -74,11 +88,15 @@ def checkCompleteness(candidateGraph):
 Define as funções de preenchimento do grafo
 '''
 #Pergunta para cada vértice a relação com cada outro vértice
+#Complexidade: O(n^2) (para cada nó, itera uma vez para cada outro nó)
 def naiveChoice(thisGraph):
+    nIterations = 0
     #Para cada nó do grafo
     for node in thisGraph.nodesList:
         #Pergunta para cada outro nó do grafo a relação entre os dois
         for neighbor in thisGraph.nodesList:
+            #A cada iteração, aumenta em 1 o número de iterações
+            nIterations += 1
             #Exceto ele próprio
             if node == neighbor:
                 continue
@@ -87,10 +105,16 @@ def naiveChoice(thisGraph):
                 thisGraph.addEdge(node, neighbor)
             else:
                 thisGraph.addEdge(neighbor, node)
+    
+    #Ao final, retorna quantos passos deu
+    return nIterations
 
 #Sempre escolhe dois vértices completamente aleatórios a cada rodada
 #Caso a relação entre os vértices já exista, simplesmente segue em frente
+#Complexidade: O(n) (para cada par de nós, itera uma vez, até parar; escalar desconhecido)
+#Checar completude ao final de cada iteração não aumenta a complexidade, só adiciona ao escalar
 def randomChoice(thisGraph):
+    nIterations = 0
     isDone = False
     while not isDone:
         #Escolhe dois nós aleatórios; garantidamente são nós distintos
@@ -103,14 +127,23 @@ def randomChoice(thisGraph):
         #a ordenação deles é exatamente essa
         thisGraph.addEdge(firstNode, secondNode)
         
+        #A cada iteração, aumenta em 1 o número de iterações
+        nIterations += 1
+
         #Ao terminar a iteração atual, checa a completude do grafo
         isDone = checkCompleteness(thisGraph)
+    
+    #Ao final, retorna quantos passos deu
+    return nIterations
 
 #Pega dois vértices arbitrários e pergunta a relação entre eles; mantém o que foi escolhido em mãos
 #e pega outro arbitrário para substituir o que não foi escolhido
 #Notavelmente, um usuário faria a escolha de A ou B, mas, para propósitos práticos, a simulação disso
 #é feita por aleatoriedade
+#Complexidade: O(n) (para cada par de nós, itera uma vez, até parar; escalar desconhecido)
+#Checar completude ao final de cada iteração não aumenta a complexidade, só adiciona ao escalar
 def snakeChoice(thisGraph):
+    nIterations = 0
     isDone = False
     #Escolhe dois nós aleatórios; garantidamente são nós distintos
     nodes = copy.deepcopy(thisGraph.nodesList)
@@ -133,16 +166,66 @@ def snakeChoice(thisGraph):
             firstNode = random.choice(nodes)
             while firstNode == secondNode:
                 firstNode = random.choice(nodes)
-            
+        
+        #A cada iteração, aumenta em 1 o número de iterações
+        nIterations += 1
+
         #Ao terminar a iteração atual, checa a completude do grafo
         isDone = checkCompleteness(thisGraph)
+    
+    #Ao final, retorna quantos passos deu
+    return nIterations
 
+
+''''
+Bloco de execução dos algoritmos; não executar ao mesmo tempo que o bloco de execução repetida
+'''
+#'''
 #Chama um método para preencher o grafo, e depois o exibe
-print("Algoritmo escolhido: NaiveChoice")
-naiveChoice(newGraph)
-#print("Algoritmo escolhido: RandomChoice")
-#randomChoice(newGraph)
+#print("Algoritmo escolhido: NaiveChoice")
+#nSteps = naiveChoice(newGraph)
+print("Algoritmo escolhido: RandomChoice")
+nSteps = randomChoice(newGraph)
 #print("Algoritmo escolhido: SnakeChoice")
-#snakeChoice(newGraph)
+#nSteps = snakeChoice(newGraph)
         
 newGraph.showGraph()
+print("\nNúmero de iterações: ")
+print(nSteps)
+#'''
+
+'''
+Bloco de execução repetida dos algoritmos; não executar ao mesmo tempo que o bloco de execução simples
+'''
+'''
+#Executa o algoritmo escolhido um número grande de vezes para normalizar o número médio de
+#iterações que cada um requer. Retorna a média de iterações
+def generateAverageRuns():
+    nIterations = 1000
+    iterationsList = []
+    
+    #Executa o algoritmo escolhido nIterations vezes
+    #print("Algoritmo escolhido: NaiveChoice")
+    print("Algoritmo escolhido: RandomChoice")
+    #print("Algoritmo escolhido: SnakeChoice")
+    for i in range(nIterations):
+        #Executar somente um de cada vez; comentar os outros
+        #currentNumber = naiveChoice(newGraph)
+        currentNumber = randomChoice(newGraph)
+        #currentNumber = snakeChoice(newGraph)
+
+        #Após calcular o número de iterações, reseta o grafo e anota quantas foram as iterações
+        #TODO Consertar resetGraph()
+        resetGraph()
+        iterationsList.append(currentNumber)
+    
+    #Calcula a média de iterações do algoritmo
+    iterationsAverage = sum(iterationsList) / len(iterationsList)
+    iterationsAverage = math.floor(iterationsAverage)
+
+    return iterationsAverage
+
+averageIterations = generateAverageRuns()
+print("\nMédia de iterações do algoritmo escolhido: ")
+print(averageIterations)
+'''
